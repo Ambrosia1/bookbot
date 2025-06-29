ffabb3c405454d0a03da900ee38d457cf2e4336e
Ambrosia1 <gallifrey0531@live.com>
2025-06-29 13:26:53 -0400

    Complete project

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from stats import get_word_count
 from stats import counting_characters
 from stats import get_sort_characters
+import sys
 
 def main():
-    book_path = "books/frankenstein.txt"
+    sys_list = len(sys.argv)
+    if sys_list != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_word_count(text)
     print(f"Found {num_words} total words")
